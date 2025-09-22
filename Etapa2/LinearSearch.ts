4723b187019be6dbdf11f2cb1490f70ec2ad8567
joaovianaAlves <joaovitorvianaalves@gmail.com>
2025-09-22 16:16:48 -0300

    fix: ajustar função de busca linear com retorno em tupla e verificação de limites

--- Etapa2/LinearSearch.ts
+++ Etapa2/LinearSearch.ts
@@ -1,11 +1,12 @@
-function LinearSearchFor(target: number): number[] {
+function LinearSearch(target: number): [number, number] {
   let array: number[] = [9, 13, 51, 6, 71, 3, 7, 1, 8];
-  for (let index = 0; index <= array.length; index++) {
+  for (let index = 0; index < array.length; index++) {
     if (array[index] === target) {
       return [target, index];
     }
   }
   console.log(`target: ${target} not found`);
-  return [-1];
+  return [-1, -1];
 }
-LinearSearchFor(1);
+let [target, index]: [number, number] = LinearSearch(1);
+console.log(`target: ${target}, postion: ${index}`);
